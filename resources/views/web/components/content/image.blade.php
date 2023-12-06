a641768179cd341176afeaff3360560b7a07fb3b
Marcel Stadelmann <m@marceli.to>
2023-12-06 09:48:36 +0100

    wip

--- resources/views/web/components/content/image.blade.php
+++ resources/views/web/components/content/image.blade.php
@@ -2,8 +2,10 @@
 <div class="swiper-slide">
   <figure>
     <picture>
+      <source media="(min-width: 1000px)" srcset="/assets/media/{{ $image }}.avif" type="image/avif">
       <source media="(min-width: 1000px)" srcset="/assets/media/{{ $image }}.webp" type="image/webp">
       <source media="(min-width: 1000px)" srcset="/assets/media/{{ $image }}.jpg">
+      <source srcset="/assets/media/{{$image}}-sm.avif" type="image/avif">
       <source srcset="/assets/media/{{$image}}-sm.webp" type="image/webp">
       <img src="/assets/media/{{$image}}-sm.jpg" width="{{$width}}" height="{{$height}}" alt="{{$alt}}" loading="lazy">
     </picture>
@@ -13,6 +15,7 @@
   <a href="/assets/media/{{$image}}-lg.jpg" data-fancybox="gallery">
     <figure>
       <picture>
+        <source srcset="/assets/media/{{$image}}-sm.avif" type="image/avif">
         <source srcset="/assets/media/{{$image}}-sm.webp" type="image/webp">
         <source srcset="/assets/media/{{$image}}-sm.jpg">
         <img src="/assets/media/{{$image}}-sm.jpg" width="{{$width}}" height="{{$height}}" alt="{{$alt}}" loading="lazy">
@@ -22,6 +25,7 @@
 @else
   <figure>
     <picture>
+      <source srcset="/assets/media/{{$image}}.avif" type="image/avif">
       <source srcset="/assets/media/{{$image}}.webp" type="image/webp">
       <source srcset="/assets/media/{{$image}}.jpg">
       <img src="/assets/media/{{$image}}.jpg" width="{{$width}}" height="{{$height}}" alt="{{$alt}}" loading="lazy">
