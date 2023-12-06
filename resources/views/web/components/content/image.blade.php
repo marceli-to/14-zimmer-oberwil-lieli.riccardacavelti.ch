4eefb35578da72109c4ab17d0b9de11210fb7514
Marcel Stadelmann <m@marceli.to>
2023-12-06 10:03:17 +0100

    wip

--- resources/views/web/components/content/image.blade.php
+++ resources/views/web/components/content/image.blade.php
@@ -1,15 +1,26 @@
 @if ($isSlide)
 <div class="swiper-slide">
+
+
   <figure>
     <picture>
+      <!-- Large Screens -->
       <source media="(min-width: 800px)" srcset="/assets/media/{{ $image }}.avif" type="image/avif">
       <source media="(min-width: 800px)" srcset="/assets/media/{{ $image }}.webp" type="image/webp">
       <source media="(min-width: 800px)" srcset="/assets/media/{{ $image }}.jpg">
-      <source srcset="/assets/media/{{$image}}-sm.avif" type="image/avif">
-      <source srcset="/assets/media/{{$image}}-sm.webp" type="image/webp">
+  
+      <!-- Small Screens -->
+      <source media="(max-width: 799px)" srcset="/assets/media/{{$image}}-sm.avif" type="image/avif">
+      <source media="(max-width: 799px)" srcset="/assets/media/{{$image}}-sm.webp" type="image/webp">
+      <source media="(max-width: 799px)" srcset="/assets/media/{{$image}}-sm.jpg">
+      
+      <!-- Fallback Image -->
       <img src="/assets/media/{{$image}}-sm.jpg" width="{{$width}}" height="{{$height}}" alt="{{$alt}}" loading="lazy">
     </picture>
-  </figure>  
+  </figure>
+  
+
+
 </div>
 @elseif ($hasLightBox)
   <a href="/assets/media/{{$image}}-lg.jpg" data-fancybox="gallery">
